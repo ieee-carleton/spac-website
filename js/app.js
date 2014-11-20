d90fb553b0aaca0ea8cde7ad1b19e137d36d0b91
System Administrator <root@owners-MacBook-Air.local>
2014-11-19 20:37:26 -0500

    started parallax stuff

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+/* detect touch */
+if("ontouchstart" in window){
+    document.documentElement.className = document.documentElement.className + " touch";
+}
+if(!$("html").hasClass("touch")){
+    /* background fix */
+    $(".parallax").css("background-attachment", "fixed");
+}
+
 /* fix vertical when not overflow
 call fullscreenFix() if .fullscreen content changes */
-
 function fullscreenFix(){
     var h = $('body').height();
     // set .fullscreen height
@@ -29,7 +37,7 @@ function backgroundResize(){
         diff = diff ? diff : 0;
         // remaining height to have fullscreen image only on parallax
         var remainingH = 0;
-        if(path.hasClass("parallax")){
+        if(path.hasClass("parallax") && !$("html").hasClass("touch")){
             var maxH = contH > windowH ? contH : windowH;
             remainingH = windowH - contH;
         }
@@ -50,3 +58,43 @@ function backgroundResize(){
 $(window).resize(backgroundResize);
 $(window).focus(backgroundResize);
 backgroundResize();
+
+/* set parallax background-position */
+function parallaxPosition(e){
+    var heightWindow = $(window).height();
+    var topWindow = $(window).scrollTop();
+    var bottomWindow = topWindow + heightWindow;
+    var currentWindow = (topWindow + bottomWindow) / 2;
+    $(".parallax").each(function(i){
+        var path = $(this);
+        var height = path.height();
+        var top = path.offset().top;
+        var bottom = top + height;
+        // only when in range
+        if(bottomWindow > top && topWindow < bottom){
+            var imgW = path.data("resized-imgW");
+            var imgH = path.data("resized-imgH");
+            // min when image touch top of window
+            var min = 0;
+            // max when image touch bottom of window
+            var max = - imgH + heightWindow;
+            // overflow changes parallax
+            var overflowH = height < heightWindow ? imgH - height : imgH - heightWindow; // fix height on overflow
+            top = top - overflowH;
+            bottom = bottom + overflowH;
+            // value with linear interpolation
+            var value = min + (max - min) * (currentWindow - top) / (bottom - top);
+            // set background-position
+            var orizontalPosition = path.attr("data-oriz-pos");
+            orizontalPosition = orizontalPosition ? orizontalPosition : "50%";
+            $(this).css("background-position", orizontalPosition + " " + value + "px");
+        }
+    });
+}
+if(!$("html").hasClass("touch")){
+    $(window).resize(parallaxPosition);
+    //$(window).focus(parallaxPosition);
+    $(window).scroll(parallaxPosition);
+    parallaxPosition();
+}
+
